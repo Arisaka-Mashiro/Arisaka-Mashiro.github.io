80979a8d1e8d997e60c59ae5473828c26e75924c
KYS <kys@KYSui-MacBook-Pro.local>
2022-03-05 16:53:20 +0900

    첫 커밋

---
+++ js/main.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+    AOS.init();
+
+    $('.download_wrapper > label').click(function (e) {
+        // 화살표를 180도 회전한다
+        $('.menu-icon-svg').toggleClass('open');
+        // 리스트를 표시한다
+        $('.download_wrapper > ul').toggleClass('open');
+    });
+
+    for (var i = 0; i < 3; i++) {
+        var card = $('div.items').clone();
+        card.appendTo('.items_container');
+    }
+
+    // var t = 0;
+    // var animate = setInterval(function () {
+    //     t = t + 60;
+    //     $('.first_image').css('transform', 'translateX('+ t +'px)');
+        
+    //     if (t > 600) {
+    //         clearInterval(animate);
+    //     }
+    // }, 33);
+});
